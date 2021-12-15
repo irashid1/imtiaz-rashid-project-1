05d6dc87116eafbe37ff77149d6f8e9864cc5554
Imtiaz <imtiaz.rashid.89@gmail.com>
2021-12-15 09:55:45 -0500

    event listener working as intended on the hamburger menu.

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const targetElement = document.querySelector('hamburger-menu');
+const targetElement = document.querySelector('.hamburger-menu');
+console.log(targetElement);
 
 targetElement.addEventListener('click', function (event) {
     console.log(event);
     if (event.target.tagName === 'I') {
-        // console.log(event.target);
-        updateToDo(event.target);
+        updateNavBar(event.target);
     }
 });
 
-function updateToDo(iElement) {
-    iElement.classList.toggle('fa-square');
-    iElement.classList.toggle('fa-check-square');
-    iElement.parentElement.classList.toggle('text-muted');
+ function updateNavBar() {
+//     iElement.classList.toggle('fa-square');
+//     iElement.classList.toggle('fa-check-square');
+//     iElement.parentElement.classList.toggle('text-muted');
 }
